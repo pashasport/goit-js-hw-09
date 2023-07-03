aeb8d58a21f9741ee80035979ebc6fc597f3a936
pashasport <123305807+pashasport@users.noreply.github.com>
2023-07-03 12:27:11 +0300

    for-check-hw-js-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -28,6 +28,7 @@ function getChangedColor() {
 
 console.log(btnStartEl);
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,95 +1,5 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import { Report } from 'notiflix/build/notiflix-report-aio';
-// import Notiflix from 'notiflix';
-// // import { update } from 'lodash';
-
-// const TIMER_DELAY = 1000;
-// let intervalId = null;
-// let selectedDate = null;
-// let currentDate = null;
-
-// // const calendarEl = document.querySelector('#datetime-picker');
-// // const btnStart = document.querySelector('[data-start]');
-// // btnStart.disabled = true;
-
-// // flatpickr(calendarEl, {
-// //   enableTime: true,
-// //   time_24hr: true,
-// //   defaultDate: new Date(),
-// //   minuteIncrement: 1,
-// //   onClose(selectedDates) {
-// //     if (selectedDates[0] < Date.now()) {
-// //       Notiflix.Notify.failure('Please, choose a date in the future!!');
-// //       btnStart.disabled = true;
-// //     } else {
-// //       Notiflix.Notify.success('Nice, you can push start!!');
-// //       btnStart.disabled = false;
-// //       const setTimer = () => {
-// //         selectedDate = selectedDates[0].getTime();
-// //         timer.start();
-// //       };
-
-// //       btnStart.addEventListener('click', setTimer);
-// //     }
-// //   },
-// // });
-
-// // const timer = {
-// //   start() {
-// //     calendarEl.disabled = true;
-// //     btnStart.disabled = true;
-// //     intervalId = setInterval(() => {
-// //       currentDate = Date.now();
-// //       const restTime = selectedDate - currentDate;
-// //       if (restTime <= 0) {
-// //         timer.stop();
-// //         Report.info('Timer end!');
-// //         return;
-// //       }
-// //       const convertTime = convertMs(restTime);
-// //       updateTimerValue(convertTime);
-// //     }, TIMER_DELAY);
-// //   },
-// //   stop() {
-// //     clearInterval(intervalId);
-// //     this.intervalId = null;
-// //     startButton.disabled = true;
-// //     calendarEl.disabled = false;
-// //   },
-// // };
-// // function convertMs(ms) {
-// //   // Number of milliseconds per unit of time
-// //   const second = 1000;
-// //   const minute = second * 60;
-// //   const hour = minute * 60;
-// //   const day = hour * 24;
-
-// //   // Remaining days
-// //   const days = Math.floor(ms / day);
-// //   // Remaining hours
-// //   const hours = Math.floor((ms % day) / hour);
-// //   // Remaining minutes
-// //   const minutes = Math.floor(((ms % day) % hour) / minute);
-// //   // Remaining seconds
-// //   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-// //   return { days, hours, minutes, seconds };
-// // }
-
-// // // btnTimeStart.addEventListener('click', btnStartTimer);
-
-// // // function btnStartTimer ()=> {
-// // //   console.log('start');
-// // // };
-
-// // // dateTimeInputEl.addEventListener('input', inputText);
-// // // function inputText(e) {
-// // //   console.log('message click', e.target.value.trim());
-// // // }
-// // console.log();
 import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
+
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 import { Report } from 'notiflix/build/notiflix-report-aio';
@@ -106,6 +16,7 @@ const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
 
 btnStartEl.disabled = true;
+
 flatpickr(calendarWindowEl, {
   enableTime: true,
   time_24hr: true,
@@ -190,5 +101,4 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
 timer.stop();
